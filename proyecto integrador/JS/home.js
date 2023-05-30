9f378678c8916a94c7a4dbef8a618649e59c3f9b
landivarrr <flandivar@udesa.edu.ar>
2023-05-30 13:10:48 -0300

    completado el alert del formulario

--- proyecto integrador/JS/home.js
+++ proyecto integrador/JS/home.js
@@ -1,8 +1,47 @@
-var buscador=document.getElementById("buscador").value;
-if (buscador.trim()===""){
-    alert("El buscador esta vacio, porfavor ingrese una busqueda");
-}else if (buscador.lenght<3){
-    alert("Debe tener almenos 3 caracteres.");
-}else{
-    busquedacompleta(buscador);
-}
+let urlcanciones="https://api.deezer.com/track/3135556"
+let section=document.querySelector(".cancioneslist")
+fetch(urlcanciones)
+.then(function(response) {
+  return response.json()
+})
+.then(function(data){
+    let result=data.contributors;
+    let canciones=""
+    for (let i = 0; i < result.length; i++){
+        canciones+=`<h2>CANCIONES</h2>
+        <ul>
+            <article>
+                <li>
+
+                </li>
+            </article>
+            <article>
+                <li>
+
+                </li>
+            </article>
+            <article>
+                <li>
+
+                </li>
+            </article>
+            <article>
+                <li>
+
+                </li>
+            </article>
+            <article>
+                <li>
+
+                </li>
+            </article>
+            <article>
+                <li>
+
+                </li>
+            </article>
+        </ul>`
+ section.innerHTML=canciones;})
+.catch(function(error) {
+  console.log("Error: " + error);
+})
